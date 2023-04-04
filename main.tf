020fb1e13916fe41fad5b3bf91bb0bf33e1e089b
raavimanikanta <raavimani123@gmail.com>
2023-04-04 17:30:44 +0530

    added childmodule2

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "newrelic_nrql_alert_condition" "foo" {
+  # for_each={for i,data in var.condition_name_querydata: i => data}    // {for i, v in var.tenants:  i => v}
+  count=length(var.condition_name_querydata)
+  type                         = "baseline"
+  account_id                   = 3627500
+  name                         = "${lookup(var.condition_name_querydata[count.index],"name")}"
+  policy_id                    = newrelic_alert_policy.main_policy.id
+  description                  = "Alert when transactions are taking too long"
+  enabled                      = true
+  runbook_url                  = "https://www.example.com"
+  violation_time_limit_seconds = 3600
+  aggregation_method           = "event_flow"
+  aggregation_delay            = 120
+  slide_by                     = 30
+
+  # baseline type only
+  baseline_direction = "upper_only"
+
+  nrql {
+    query="${lookup(var.condition_name_querydata[count.index],"Nrql")}"
+    # query = "SELECT percentile(duration, 95) FROM Transaction WHERE appName = 'FoodMe'"
+  }
+
+  critical {
+    operator              = "above"
+    threshold             ="${lookup(var.condition_name_querydata[count.index],"critical_threshold")}"
+    threshold_duration    = "${lookup(var.condition_name_querydata[count.index],"critical_threshold_duration")}"
+    threshold_occurrences = "all"
+  }
+
+  warning {
+    operator              = "above"
+    threshold             ="${lookup(var.condition_name_querydata[count.index],"warning_threshold")}"
+    threshold_duration    = "${lookup(var.condition_name_querydata[count.index],"warning_threshold_duration")}"
+    threshold_occurrences = "all"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "condition_name_querydata" {
+  # type=list(object({
+  #   name=string
+  #   Nrql=string
+  #   critical_threshold=number
+  #   critical_threshold_duration=number
+  #   warning_threshold=number
+  #   warning_threshold_duration=number
+  # }))
+  type=list(any)
+}
+
+# variable "critical_threshold_values" {
+#   type=list(object({
+   
+   
+#   }))
+# }
+
+# variable "warning_threshold_values" {
+#   type=list(object({
+   
+#    }))
+# }
