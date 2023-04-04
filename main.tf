d8c3df0d1b10fe08c0cf66bae02e1344966a731d
raavimanikanta <raavimani123@gmail.com>
2023-04-04 18:03:17 +0530

    added policy details

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
+data "newrelic_alert_policy" "foo" {
+  name = "data_source_policy"
+}
+
+
 resource "newrelic_nrql_alert_condition" "foo" {
   # for_each={for i,data in var.condition_name_querydata: i => data}    // {for i, v in var.tenants:  i => v}
+  
+  
   count=length(var.condition_name_querydata)
   type                         = "baseline"
   account_id                   = 3627500
   name                         = "${lookup(var.condition_name_querydata[count.index],"name")}"
-  policy_id                    = newrelic_alert_policy.main_policy.id
+  policy_id                    = data.newrelic_alert_policy.foo.id
   description                  = "Alert when transactions are taking too long"
   enabled                      = true
   runbook_url                  = "https://www.example.com"
