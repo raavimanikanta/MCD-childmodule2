a0fa3e243ed94d34647d3defe36a266ae9541b79
raavimanikanta <raavimani123@gmail.com>
2023-04-05 15:15:27 +0530

    added changes

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the New Relic provider
 provider "newrelic" {
-   account_id =3627500
+  account_id =3627500
   api_key = "NRAK-A7QTJY3NOV9VF3HCAAU2UVJB7PX"    # Usually prefixed with 'NRAK'
   region = "US"                    # Valid regions are US and EU
 }
