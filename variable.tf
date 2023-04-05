9ccf9f07e9034866b60f8cf247dd797afce37e4a
raavimanikanta <raavimani123@gmail.com>
2023-04-05 20:42:07 +0530

    added changes in child module

--- variable.tf
+++ variable.tf
@@ -1,28 +1,8 @@
 variable "condition_name_querydata" {
-  # type=list(object({
-  #   name=string
-  #   Nrql=string
-  #   critical_threshold=number
-  #   critical_threshold_duration=number
-  #   warning_threshold=number
-  #   warning_threshold_duration=number
-  # }))
-  type=list(any)
+     type=list(any)
 }
 
 variable "policy_id" {
   type = any
   }
 
-# variable "critical_threshold_values" {
-#   type=list(object({
-   
-   
-#   }))
-# }
-
-# variable "warning_threshold_values" {
-#   type=list(object({
-   
-#    }))
-# }
